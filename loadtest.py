e8c9ccb72a6821f3534515d59d58be2d8e355089
Compieta, Paolo <paolo.competa@accenture.com>
2023-11-27 20:03:18 +0100

    some rename adding 'busy_cpu_sec' for keeping cpu actively busy for N secs

--- loadtest.py
+++ loadtest.py
@@ -7,10 +7,7 @@ import datetime
 import simple_score
 
 
-def waste_seconds():
-    """
-    taking approx 0.7-0.9 sec
-    """
+def keep_cpu_busy(seconds_of_computation: float = 1.0):
     import hashlib
     import os
 
@@ -19,11 +16,12 @@ def waste_seconds():
         hasher.update(inner_data)
         return hasher.hexdigest()
 
-    data = os.urandom(250 * 1024 * 1024)  # Generate a large block of data: 250 MB of data, approx 2s to be hashed
+    seconds_to_waste_int = int(seconds_of_computation * 125 * 1024 * 1024)
+    data = os.urandom(seconds_to_waste_int)  # Generate large block of data: hashing at 125 MB/sec
     hash_function(data)
 
 
-def process_single(responses, question: str, dryrun: bool):
+def process_single(responses, question: str, dryrun: bool, busy_cpu_sec: float):
     t_start = perf_counter()
     start_time = datetime.datetime.now().isoformat(sep=" ", timespec='milliseconds')
 
@@ -31,38 +29,38 @@ def process_single(responses, question: str, dryrun: bool):
         json_result = simple_score.process(model_ready["model"], model_ready["tokenizer"], question)
         print(json_result)
     else:
-        waste_seconds()
+        keep_cpu_busy(busy_cpu_sec)
 
     t_stop = perf_counter()
     end_time = datetime.datetime.now().isoformat(sep=" ", timespec='milliseconds')
     responses.append({"elapsed": t_stop - t_start, "start_time": start_time, "end_time": end_time})
 
 
-def sequential_test(many: int, question: str, dryrun: bool):
+def sequential_test(many: int, question: str, dryrun: bool, busy_cpu_sec: float):
     responses = []
     t_begin = perf_counter()
 
     for _ in range(many):
-        process_single(responses, question, dryrun)
+        process_single(responses, question, dryrun, busy_cpu_sec)
 
     t_end = perf_counter()
     total_elapsed = t_end - t_begin
     throughput = many / total_elapsed
     print(f"""SEQUENTIAL TEST
-          total_elapsed={total_elapsed:.1f}
-          throughput={throughput:.1f}
+          total_elapsed={total_elapsed:.1f}s
+          throughput={throughput:.1f} job/s
           """)
     print_df_as_table(responses)
 
 
-def multithread_test(many: int, question: str, dryrun: bool, delay: int = 0):
+def multithread_test(many: int, question: str, dryrun: bool, delay: int, busy_cpu_sec: float):
     responses = []
     t_begin = perf_counter()
 
     with ThreadPoolExecutor(max_workers=many) as executor:
         futures = []
         for _ in range(many):
-            futures.append(executor.submit(process_single, responses, question, dryrun))
+            futures.append(executor.submit(process_single, responses, question, dryrun, busy_cpu_sec))
             time.sleep(delay)
         wait(futures, return_when="FIRST_EXCEPTION")
 
@@ -70,20 +68,21 @@ def multithread_test(many: int, question: str, dryrun: bool, delay: int = 0):
     total_elapsed = t_end - t_begin
     throughput = many / total_elapsed
     print(f"""THREADPOOL TEST
-          total_elapsed={total_elapsed:.1f}
-          throughput={throughput:.1f}
+          total_elapsed = {total_elapsed:.1f}s
+          throughput = {throughput:.1f} job/s
+          delay = {delay:.1f}s
           """)
     print_df_as_table(responses)
 
 
-def multiprocess_test(many: int, question: str, dryrun: bool, delay: int = 0):  # NOT WORKING YET
+def multiprocess_test(many: int, question: str, dryrun: bool, delay: int, busy_cpu_sec: float):  # NOT WORKING YET
     responses = []
     t_begin = perf_counter()
 
     futures = []
     with ProcessPoolExecutor() as executor:
         for _ in range(many):
-            futures.append(executor.submit(process_single, responses, question, dryrun))
+            futures.append(executor.submit(process_single, responses, question, dryrun, busy_cpu_sec))
             time.sleep(delay)
         wait(futures, return_when="FIRST_EXCEPTION")
 
@@ -112,6 +111,7 @@ if __name__ == '__main__':
     parser.add_argument('--delay', dest='delay', default=1, type=int, choices=range(1, 10), help='seconds of delay between calls')
     parser.add_argument('--question', dest='question', default="what is AI Navigator?")
     parser.add_argument('--dryrun', dest='dryrun', type=bool, default=False)
+    parser.add_argument('--busy_cpu_sec', dest='busy_cpu_sec', default=2, type=int, help='secs of compute in case of dryrun')
 
     args = parser.parse_args()
     print(args)
@@ -120,11 +120,11 @@ if __name__ == '__main__':
         model_ready = simple_score.init(args.model_path)
 
     print("=======================================================================")
-    sequential_test(args.many, args.question, args.dryrun)
+    sequential_test(args.many, args.question, args.dryrun, args.busy_cpu_sec)
     print("=======================================================================")
-    multithread_test(args.many, args.question, args.dryrun, 0)
+    multithread_test(args.many, args.question, args.dryrun, 0, args.busy_cpu_sec)
     print("=======================================================================")
-    multithread_test(args.many, args.question, args.dryrun, args.delay)
+    multithread_test(args.many, args.question, args.dryrun, args.delay, args.busy_cpu_sec)
     # print("=======================================================================")
     # multiprocess_test(args.many, args.question, args.dryrun, 0)
     # print("=======================================================================")
